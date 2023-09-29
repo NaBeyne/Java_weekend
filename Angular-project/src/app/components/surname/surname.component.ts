6e7f4322da2f5579b8b0fb71c8cdab3a3452c361
Rebecca Tang <rebecca.tang@axxes.com>
2023-09-29 15:52:45 +0200

    renew letter every 50ms

--- Angular-project/src/app/components/surname/surname.component.ts
+++ Angular-project/src/app/components/surname/surname.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-surname',
   templateUrl: './surname.component.html',
   styleUrls: ['./surname.component.scss'],
 })
-export class SurnameComponent implements OnInit {
+export class SurnameComponent implements OnInit, OnDestroy {
   letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   currentLetter = '';
   surname = '';
+  subscription: Subscription;
+  pauze = false;
+  btnLabel = 'New letter';
 
   ngOnInit(): void {
     this.randomLetter();
+  }
+
+  startOrPauze(): void {
+    if (!this.pauze) this.startRandom();
+    else this.pauzeRandom();
+  }
+
+  startRandom(): void {
+    this.subscription = interval(50).subscribe(() => this.randomLetter());
+    this.pauze = true;
+    this.btnLabel = 'Pauze';
+  }
+
+  pauzeRandom(): void {
+    this.subscription.unsubscribe();
+    this.pauze = false;
+    this.btnLabel = 'New letter';
   }
 
   randomLetter(): void {
@@ -22,5 +43,9 @@ export class SurnameComponent implements OnInit {
 
   addToName(): void {
     this.surname += this.currentLetter;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
