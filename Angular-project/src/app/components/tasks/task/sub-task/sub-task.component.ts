fe72b2db60374921725101cadeba1ea082b3804e
sdefour <stijn.defour@rekencentra.be>
2023-09-29 16:45:35 +0200

    sub tasks

--- Angular-project/src/app/components/tasks/task/sub-task/sub-task.component.ts
+++ Angular-project/src/app/components/tasks/task/sub-task/sub-task.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
-import {SubTask} from "../../../../state/task/task.reducer";
+import {SubTask, Task} from "../../../../state/task/task.reducer";
+import {toggleSubTask} from "../../../../state/task/task.actions"
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-sub-task',
@@ -10,5 +12,18 @@ export class SubTaskComponent {
 
   @Input()
   subtask: SubTask;
+  @Input()
+  task: Task;
+
+  constructor(private store: Store) {
+  }
+
+
+  toggle(): void {
+    console.log();
+    this.store.dispatch(toggleSubTask({payload: {
+        task: this.task, subtask: this.subtask
+    }}));
+  }
 
 }
--- Angular-project/src/app/state/task/task.actions.ts
+++ Angular-project/src/app/state/task/task.actions.ts
@@ -5,6 +5,6 @@ export const addTask = createAction('add task', props<{ payload: Task }>());
 export const removeTask = createAction('remove task', props<{ payload: Task }>());
 
 export const addSubTask = createAction('add subtask', props<{ payload: { task: Task, subtask: SubTask } }>());
-export const removeSubTask = createAction('remove subtask', props<{ payload: { task: Task, subtask: SubTask } }>());
+export const removeSubTask = createAction('remove subtask', props<{ payload: {task: Task, subtask: SubTask} }>());
 
-export const toggleSubTask = createAction('toggle subtask', props<{ payload: { task: Task, subtask: SubTask } }>());
+export const toggleSubTask = createAction('toggle subtask', props<{ payload: {task: Task, subtask: SubTask} }>());
